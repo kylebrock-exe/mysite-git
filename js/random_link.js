effbda034e475779d636bc5e6cd51948abab04be
kylebrock-exe <65414965+kylebrock-exe@users.noreply.github.com>
2020-07-13 19:55:10 -0400

    Java Fix

--- js/random_link.js
+++ js/random_link.js
@@ -1,17 +1,17 @@
-object.addEventListener("click", random_link);
+var randomLinkButton = document.getElementById('random-link');
 
-function random_link() {
+randomLinkButton.addEventListener('click', function()) {
 	var random=Math.round(Math.random()*9)
-	var link1="https://youtu.be/gsGujOfkixc"
-	var link2="https://youtu.be/MItpz6Vehow"
-	var link3="https://youtu.be/K8N5Hm6IGGs"
-	var link4="https://youtu.be/LVNpW5SSTuk"
-	var link5="https://youtu.be/psNEie5HKDg"
-	var link6="https://youtu.be/qAL-UKGbDbI"
-	var link7="https://youtu.be/XU2d6wQBBHQ"
-	var link8="https://youtu.be/9L8LO3a_gGI"
-	var link9="https://youtu.be/9YOJrwVFMN0"
-	var link10="https://youtu.be/VLOD0B6QDP0"
+	var link1="https://youtu.be/gsGujOfkixc";
+	var link2="https://youtu.be/MItpz6Vehow";
+	var link3="https://youtu.be/K8N5Hm6IGGs";
+	var link4="https://youtu.be/LVNpW5SSTuk";
+	var link5="https://youtu.be/psNEie5HKDg";
+	var link6="https://youtu.be/qAL-UKGbDbI";
+	var link7="https://youtu.be/XU2d6wQBBHQ";
+	var link8="https://youtu.be/9L8LO3a_gGI";
+	var link9="https://youtu.be/9YOJrwVFMN0";
+	var link10="https://youtu.be/VLOD0B6QDP0";
 	if (random==0)
 		window.location=link1
 	else if (random==1)
